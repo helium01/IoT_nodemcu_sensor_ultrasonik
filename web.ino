d589a375a05e0c6616bc35f8cc898e9930ff89cf
helium01 <fajarw935@gmail.com>
2022-06-01 03:30:29 +0700

    menambahkan koding arduino

---
+++ web.ino
@@ -0,0 +1,60 @@
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+ 
+// Replace with your network credentials
+const char* ssid = "Farel";
+const char* password = "11111111";
+ 
+ESP8266WebServer server(80);   //instantiate server at port 80 (http port)
+ 
+String page = "";
+int pinbuzer = D0;
+#define pinecho D3
+#define pintrig D4
+#define servopin D2
+int jarakmax=300;
+int jarakmin=2;
+long waktu,jarak;
+
+
+void setup(void){
+  
+   
+  delay(1000);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password); //begin WiFi connection
+  Serial.println("");
+ 
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+   
+  server.on("/", [](){
+    server.send(200, "text/html", page);
+  });
+  server.on("/LEDOn", [](){
+    server.send(200, "text/html", page);
+    digitalWrite(LEDPin, HIGH);
+    delay(1000);
+  });
+  server.on("/LEDOff", [](){
+    server.send(200, "text/html", page);
+    digitalWrite(LEDPin, LOW);
+    delay(1000); 
+  });
+  server.begin();
+  Serial.println("Web server started!");
+}
+ 
+void loop(void){
+  server.handleClient();
+}
